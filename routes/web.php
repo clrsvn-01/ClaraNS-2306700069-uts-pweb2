797ed09cd46d69b78b5f3903d4f620548894715a
clrsvn-01 <clarasavena66@gmail.com>
2026-05-28 22:32:04 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,9 @@
 
 use App\Http\Controllers\EmployeeController;
 
-// Halaman utama (Landing Page)
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Halaman data karyawan
 Route::get('/employees', [EmployeeController::class, 'index']);
 Route::get('/user/{id}', [EmployeeController::class, 'show']);
